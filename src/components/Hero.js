bd2d931f55522c9361cfebc62ec7c849704dfe1f
shrijo <jonas.shriqui@gmail.com>
2019-10-23 11:32:15 +0200

    outer wrapper of hero removed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,9 @@ import HeroStyles from './Hero.module.css'
 
 
 const Hero = () => (
-  <div className={HeroStyles.outerWrapper}>
     <Div100vh className={HeroStyles.container}>
       <h1 className={HeroStyles.logo}>KARATE STUDIO</h1>
     </Div100vh>
-  </div>
 )
 
 export default Hero;
